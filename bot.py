4b5285bafb62ac00afc285f80cd4019d32bb3ed7
feverlxadxut <104371135+feverlxadxut@users.noreply.github.com>
2022-06-04 12:36:32 +0200

    add report command
    )(

--- bot.py
+++ bot.py
@@ -23,6 +23,22 @@ def _load_Cogs(): # Подгрузка когов
                 client.load_extension(f'cogs.{filename[:-3]}')
             else:
                 client.load_extension(f'cogs.{filename}')
+                
+@bot.slash_command(name="report")
+async def report(ctx, user: disnake.Member, reason):
+    channel = await client.fetch_channel(981143262981152769)
+    await ctx.message.delete()
+    msg = await channel.send(embed=disnake.Embed(
+        title='Жалоба',
+        description=f'{ctx.author.mention} отправил жалобу на {user.mention}').
+                             add_field(name='Причина', value=f'{reason}'))
+
+    def check(r, user):
+        return r.emoji == '❌' and user != client.user
+
+    await msg.add_reaction("❌")
+    reaction = await client.wait_for('reaction_add', check=check)
+    await msg.delete()
 
 if __name__ == "__main__":
     _load_Cogs()
